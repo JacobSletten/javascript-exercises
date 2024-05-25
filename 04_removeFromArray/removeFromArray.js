e2e1a322ec85a99f115476e95789132e5245b2f1
JacobSletten <slettenj2628@my.uwstout.edu>
2024-05-24 21:04:37 -0500

    Completed JavaScript Exercise Four

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr) {
+    let targets = Array.from(arguments);
+    
+    let insertion = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (!targets.some(a => a === arr[i])) {
+            arr[insertion] = arr[i];
+            insertion++;
+        }
+    }
+    return arr.slice(0,insertion);
 };
 
 // Do not edit below this line
