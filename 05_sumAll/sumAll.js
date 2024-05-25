9d57d005394b03215d2ea61dab3b4b0138cc8b58
JacobSletten <slettenj2628@my.uwstout.edu>
2024-05-24 21:12:57 -0500

    JavaScript Exercise Five Complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(left, right) {
+    let sum = 0;
 
+    if (left > right) {
+        let temp = right;
+        right = left;
+        left = temp;
+    }
+
+    if ( (left < 0 || right < 0) || (typeof(left) != "number" || typeof(right) != "number") ) {
+        return "ERROR"
+    }
+
+    for (let i = left; i <= right; i++) {
+        sum = sum + i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
