beec2d9be4a31a1051612793e1c6d39560e7fdd0
JacobSletten <slettenj2628@my.uwstout.edu>
2024-05-24 20:45:44 -0500

    JavaScript Exercise Three Complete

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,18 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let arr = str.split('');
 
+    let left = 0;
+    let right = arr.length - 1;
+
+    while (left < right) {
+        let temp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = temp;
+        left++;
+        right--;
+    }
+
+    return arr.join('');
 };
 
 // Do not edit below this line
